f3671effbfb2d7c91a794689ff66cbe88675d359
Lazar Godjirov <lazargodzirov@yahoo.com>
2023-03-02 19:51:21 +0100

    show and hide btn

--- add.js
+++ add.js
@@ -2,4 +2,22 @@ function shareEvent(){
    alert('my alert message') 
 }
 
-document.getElementById("share-button").addEventListener("click", shareEvent);
+const table= document.querySelector('table');
+
+  // Hide the table initially
+  table.style.display = 'none';
+
+  // Get the button element
+  const showTableBtn = document.getElementById('showTableBtn');
+
+  // Add a click event listener to the button
+  showTableBtn.addEventListener('click', function() {
+    // Toggle the display property of the table
+    if (table.style.display === 'none') {
+      table.style.display = 'table';
+      showTableBtn.textContent = 'Hide Recipes';
+    } else {
+      table.style.display = 'none';
+      showTableBtn.textContent = 'Show Recipes';
+    }
+  });
